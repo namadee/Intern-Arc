48ca4024cefea8fdcbefcfba938fd84a8bd90ead
ruchira-bogahawatta <ruchira.bogahawatta@gmail.com>
2022-12-06 00:00:34 +0530

    changes in session helper

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -84,36 +84,3 @@ window.addEventListener("DOMContentLoaded", function () {
 
   getNavigationMenu(navigationMenu); //Calling the function
 });
-
-// window.onload = function navigationPDC() {
-
-//     let pdcMenu = [
-//       ["#", "dashboard", "Dashboard"],
-//       ["#", "contact_phone", "Manage Company"],
-//       ["#", "school", "Manage Student"],
-//       ["#", "work", "Job Roles"],
-//       ["#", "text_to_speech", "Advertisements"],
-//       ["#", "compare_arrows", "Requests"],
-//       ["#", "manage_accounts", "Profile"],
-//     ];
-
-//     for (let i = 0; i < pdcMenu.length; i++) {
-//       // get the size of the inner array
-//       // loop the inner array
-//       document.getElementById("navbarItems").innerHTML +=
-//         ` <li>
-//           <a href="` +
-//         pdcMenu[i][0] +
-//         `">
-//               <span class="material-symbols-rounded">
-//               ` +
-//         pdcMenu[i][1] +
-//         `
-//               </span>
-//               <p>` +
-//         pdcMenu[i][2] +
-//         `</p>
-//           </a>
-//       </li>`;
-//     }
-//   };
