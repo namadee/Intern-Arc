77dbfb987e98d43162b3a4d781aa9dc987561407
namadee <namadeeshakya1819@gmail.com>
2022-12-05 22:36:55 +0530

    profile view

---
+++ app/views/pages/company/profileView.php
@@ -0,0 +1,36 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+<?php require APPROOT . '/views/includes/navbar.php'; ?>
+
+<section id="profile_page" class="main-content profile-main-content"> 
+    <div class="profile-main-container">
+        <div class="profile-left">
+            <div class="profile-left-top">
+                <h2>Company Profile</h2>
+                <img src="<?php echo URLROOT . 'img/linkedIn_icon.png' ?>">
+            </div>
+            <img class="profileImg" src="<?php echo URLROOT . 'img/profile-img/profile-icon.svg' ?>">
+            <h1>Welcome To Virtusa</h1>
+            <p>Sparking innovation, one sprint at a time.</p>
+            <p>Eshtablished Date: 2000-08-08</p>
+
+            <div class="profile-bottom">
+                <span class="material-symbols-outlined">location_on</span>
+                <p>752 , Dr Danister De Silva Mawatha, Colombo 09</p>
+            </div>   
+
+        </div>
+        <div class="profile-right">
+            <p class="description">
+            Virtusa help clients change, disrupt, and unlock new value that surpasses 
+            their wildest expectations not just to reach our best, but to redefine yours.
+            </p>
+
+            <button class="profile-btn" >Visit Website</button>
+
+        </div>
+
+    </div> 
+
+</section>
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
