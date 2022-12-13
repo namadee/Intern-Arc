a8dbcec6a5c7e1644b289245fabe90b6ef35cec5
namadee <namadeeshakya1819@gmail.com>
2022-12-13 11:06:35 +0530

    Adding error view

--- app/views/pages/errorView.php
+++ app/views/pages/errorView.php
@@ -1,8 +1,9 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 
 <section class="error-page-container">
-    <br><br>
-<h2>Ooops page is notf found</h2>
+    
+    <h1>Oops page not found </h1>   
+    <img class="four04" src="<?php echo URLROOT. "img/error-img.svg" ?>">
 
 </section>
 
