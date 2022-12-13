e4c9aa5a1701ffda2d429543c5025d8465d94ff9
namadee <namadeeshakya1819@gmail.com>
2022-12-13 09:37:39 +0530

    Adding error views

---
+++ app/views/pages/noResultView.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+
+<section class="error-page-container">
+    <h1>Oops No result found </h1>
+    <img src="<?php echo URLROOT . "img/no-response-img.svg"?>">
+
+</section>
+
+
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
+
+
