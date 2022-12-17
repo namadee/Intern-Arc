bcbc705937560b8f9ea4085e805022888c31334d
ruchira-bogahawatta <ruchira.bogahawatta@gmail.com>
2022-12-17 22:42:12 +0530

    Adjusted error pages

--- app/controllers/Errors.php
+++ app/controllers/Errors.php
@@ -13,22 +13,11 @@ class Errors extends BaseController
         $this->view('error');
     }
 
-    public function error404()
+
+    public function noAccess()
     {
-        $data = [
-            'title' => 'page not found'
-        ];
 
-        $this->view('error', $data);
-    }
-
-    public function error403()
-    {
-        $data = [
-            'title' => 'no result'
-        ];
-
-        $this->view('noResult', $data);
+        $this->view('noAccess');
     }
 
 
--- app/views/pages/errorView.php
+++ app/views/pages/errorView.php
@@ -1,10 +1,13 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 
-<section class="error-page-container">
-    
-    <h1>Oops page not found </h1>   
-    <img class="four04" src="<?php echo URLROOT. "img/error-img.svg" ?>">
+<section class="error-page-container display-flex-row">
 
+    <img class="four04" src="<?php echo URLROOT . "img/error-icon.svg" ?>">
+    <div class="display-flex-col">
+        <h1>Ooops Page Not Found !!!</h1>
+        <p>Seems like the page you are looking for is not available.</p>
+        <a href="<?php echo URLROOT."users"; ?>">Click Here to Redirect</a>
+    </div>
 </section>
 
 
