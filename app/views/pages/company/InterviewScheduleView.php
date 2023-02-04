a2e6dc6ac939309f68c2797dda348454b9350fd2
namadee <namadeeshakya1819@gmail.com>
2023-02-04 18:51:08 +0530

    Adding schedule UIs

---
+++ app/views/pages/company/InterviewScheduleView.php
@@ -0,0 +1,84 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+<link rel="stylesheet" href="<?php echo URLROOT; ?>css/company.css">
+<script src="<?php echo URLROOT; ?>js/schedule.js" defer></script>
+<?php require APPROOT . '/views/includes/navbar.php'; ?>
+
+<section id="create-sche_page" class="main-content create-sche-main-content"> 
+    <div class="create-sche-main-container">
+        
+    <h2>Web development Internship Schedule</h2>
+
+<button class="collapsible"><span class="material-symbols-outlined">
+chevron_right
+</span>02/02/2022 - Monday - 9.30 to 12.00 </button>
+<div class="content">
+  <table>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Not Selected</td>
+    </tr>
+  </table>
+</div>
+<button class="collapsible"><span class="material-symbols-outlined">
+chevron_right
+</span>02/02/2022 - Monday - 9.30 to 12.00 </button>
+<div class="content">
+  <table>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+  </table>
+</div>
+<button class="collapsible"><span class="material-symbols-outlined">
+chevron_right
+</span>02/02/2022 - Monday - 9.30 to 12.00 </button>
+<div class="content">
+  <table>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+    <tr>
+      <td>9.30 AM - 10.00 AM </td>
+      <td>Namadee Shakya</td>
+      <td><a href="" class="common-blue-btn">Cancel</a></td>
+    </tr>
+  </table>
+</div>
+
+
+
+   
+    </div>
+
+</section>
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
