51665aa17c9f2cc55bf8ea781107a695119b6ed3
namadee <namadeeshakya1819@gmail.com>
2023-05-09 13:13:45 +0530

    changing header of advertisement

--- app/views/pages/company/advertisementView.php
+++ app/views/pages/company/advertisementView.php
@@ -3,23 +3,29 @@
 <?php require APPROOT . '/views/includes/navbar.php'; ?>
 
 <section id="advertisement_page" class="main-content advertisement-main-content">
-    <?php flashMessage('student_request_msg'); ?> 
+    <?php flashMessage('student_request_msg'); ?>
     <div class="advertisement-main-container">
         <div class="ad-header">
             <div class="display-flex-row ad-header-top">
                 <img class="ucsc-logo" src="<?php echo URLROOT ?>img/ucsc-logo.png">
-                <h1>University of Colombo School Of Computing</h1>
-                <img class="internarc-logo" src="<?php echo URLROOT ?>img/logo.png">
-            </div>
-            <div class="display-flex-col ad-header-bottom">
-                <h2>UCSC INDUSTRIAL PLACEMENTS-2024</h2>
-                <h3>Placement Advertisement</h3>
+                <!-- <h1>University of Colombo School Of Computing</h1> -->
+                <!-- <img class="internarc-logo" src="<?php echo URLROOT ?>img/logo.png"> -->
+
+                <div class="display-flex-col ad-header-bottom">
+                    <h1>University of Colombo School Of Computing</h1>
+                    <h2>UCSC INDUSTRIAL PLACEMENTS-2024</h2>
+                    <h3>Placement Advertisement</h3>
+                </div>
             </div>
         </div><br><br>
-        
-       <div class=" display-flex-row advertisement-container-body">
-        <div class="body-left">
-            <ul class="display-flex-col">
+
+        <div class=" display-flex-row advertisement-container-body">
+            <div class="body-left">
+                <ul class="display-flex-col">
+                    <li class="display-flex-row">
+                        <p class="profile-item">Company</p>
+                        <span></span>
+                    </li>
                     <li class="display-flex-row">
                         <p class="profile-item">Position</p>
                         <span><?php echo $data['position'] ?></span>
@@ -42,37 +48,36 @@
                             <span><?php echo $data['internship_start']  ?></span>
                             <span><?php echo $data['internship_end'] ?></span>
                         </div>
-                        
-                    </li>
-                    
-                </ul> 
-            </div>
-       
 
-        <div class="body-right">
-            <div class="display-flex-col job-description">
+                    </li>
+
+                </ul>
+            </div>
+
+
+            <div class="body-right">
+                <div class="display-flex-col job-description">
                     <h3>Job Description</h3><br>
                     <div class="display-flex-col job-description-items">
                         <p><?php echo $data['job_description'] ?></p>
                     </div>
-            </div><br>
-            <div class="display-flex-col job-description">
+                </div><br>
+                <div class="display-flex-col job-description">
                     <h3>Requirements</h3><br>
                     <div class="display-flex-col job-description-items">
                         <p><?php echo $data["requirements"] ?></p>
                     </div>
+                </div>
             </div>
-       </div>
-       
-       </div>
-        <a href="<?php 
-            if($_SESSION['user_role'] == 'company'){
-                echo URLROOT . 'requests/showRequestsByAd/'.$data['advertisement_id'];
-            }else if($_SESSION['user_role'] == 'student'){
-                echo URLROOT . 'requests/addStudentRequest/' . $data['advertisement_id'];
 
-            } ?>" class="common-blue-btn apply-btn"><?php echo $data['button_name'] ?></a>
-    </div> 
+        </div>
+        <a href="<?php
+                    if ($_SESSION['user_role'] == 'company') {
+                        echo URLROOT . 'requests/showRequestsByAd/' . $data['advertisement_id'];
+                    } else if ($_SESSION['user_role'] == 'student') {
+                        echo URLROOT . 'requests/addStudentRequest/' . $data['advertisement_id'];
+                    } ?>" class="common-blue-btn apply-btn"><?php echo $data['button_name'] ?></a>
+    </div>
 
 </section>
 
